17f792f90e927c17c0cf8c3d3c8bdf7b81947304
highqualitybean <32654356+highqualitybean@users.noreply.github.com>
2019-02-28 08:38:33 -0500

    Update Pig.java

--- Pig/src/Pig/Pig.java
+++ Pig/src/Pig/Pig.java
@@ -1,5 +1,17 @@
-package Pig;
-
-public class Pig {
-
+package Pig; //java web start?
+import javax.swing.*;
+public class Pig 
+{
+	private static void initGUI() //creates the GUI for the game
+	{
+		JFrame frame = new JFrame("Frame"); //creates the jframe
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //closes the program when the frame is exited
+		frame.pack(); //resizes the frame
+		frame.setVisible(true); //shows the frame
+	}
+	
+	public static void main(String [] args)
+	{
+		initGUI(); //creates the GUI
+	}
 }
