1011b559d8e92f9f79b0137f2f4c386650505bae
highqualitybean <32654356+highqualitybean@users.noreply.github.com>
2019-02-28 08:45:20 -0500

    Update Pig.java

--- Pig/src/Pig/Pig.java
+++ Pig/src/Pig/Pig.java
@@ -1,11 +1,16 @@
 package Pig; //java web start?
+import java.awt.BorderLayout;
+
 import javax.swing.*;
 public class Pig 
 {
 	private static void initGUI() //creates the GUI for the game
 	{
 		JFrame frame = new JFrame("Frame"); //creates the jframe
+		frame.setUndecorated(true);
+		frame.getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //closes the program when the frame is exited
+		frame.getContentPane().add("", BorderLayout.CENTER);
 		frame.pack(); //resizes the frame
 		frame.setVisible(true); //shows the frame
 	}
