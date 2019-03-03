6d07c4a3f9729beb5687b2c3f0aa42244333d30c
highqualitybean <32654356+highqualitybean@users.noreply.github.com>
2019-03-03 18:47:58 -0500

    Update Pig.java

--- Pig/src/Pig/Pig.java
+++ Pig/src/Pig/Pig.java
@@ -1,6 +1,8 @@
 package Pig; //@notesForMe: java web start?
-import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import javax.swing.*;
+
 public class Pig 
 {
 	private static void initGUI() //creates the GUI for the game
@@ -9,7 +11,45 @@ public class Pig
 		frame.setUndecorated(true);
 		frame.getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //closes the program when the frame is exited
-		frame.getContentPane().add("", BorderLayout.CENTER);
+		frame.setLayout(new GridBagLayout());
+		
+		JLabel userOneLabel = new JLabel("User One");
+		JLabel currentRoll = new JLabel("0");
+		JLabel userTwoLabel = new JLabel("User Two");
+		JLabel CurrentPot = new JLabel("Pot");
+		JButton rollButton = new JButton("Roll Again");
+		JButton takeButton = new JButton("Keep");
+		
+		GridBagConstraints userOneCon = new GridBagConstraints();
+		userOneCon.gridx = 0;
+		userOneCon.gridy = 0;
+		frame.add(userOneLabel, userOneCon);
+		
+		GridBagConstraints rollCon = new GridBagConstraints();
+		rollCon.gridx = 1;
+		rollCon.gridy = 0;
+		frame.add(currentRoll, rollCon);
+		
+		GridBagConstraints userTwoCon = new GridBagConstraints();
+		userTwoCon.gridx = 2;
+		userTwoCon.gridy = 0;
+		frame.add(userTwoLabel, userTwoCon);
+		
+		GridBagConstraints rollButtonCon = new GridBagConstraints();
+		rollButtonCon.gridx = 0;
+		rollButtonCon.gridy = 1;
+		frame.add(rollButton, rollButtonCon);
+		
+		GridBagConstraints potCon = new GridBagConstraints();
+		potCon.gridx = 1;
+		potCon.gridy = 1;
+		frame.add(CurrentPot, potCon);
+		
+		GridBagConstraints takeCon = new GridBagConstraints();
+		takeCon.gridx = 2;
+		takeCon.gridy = 1;
+		frame.add(takeButton, takeCon);
+		
 		frame.pack(); //resizes the frame
 		frame.setVisible(true); //shows the frame
 	}
