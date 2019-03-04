5a21f6d0ddc090c4c0e2b9781310fdfdd5ed5f57
highqualitybean <32654356+highqualitybean@users.noreply.github.com>
2019-03-03 22:25:52 -0500

    Update Pig.java

--- Pig/src/Pig/Pig.java
+++ Pig/src/Pig/Pig.java
@@ -2,12 +2,29 @@ package Pig; //@notesForMe: java web start?
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 import javax.swing.*;
 
 public class Pig 
 {
-	private static void initGUI() //creates the GUI for the game
+	static int currentRollValue = 0;
+	static int potValue = 0;
+	static boolean currentUser = true; //true = one / red
+	static int userOneScore = 0;
+	static int userTwoScore = 0;
+	
+	static JLabel userOneLabel = new JLabel("User One");
+	static JLabel currentRoll = new JLabel("0");
+	static JLabel userTwoLabel = new JLabel("User Two");
+	static JLabel currentPot = new JLabel("Pot");
+	static JButton rollButton = new JButton("Roll Again");
+	static JButton takeButton = new JButton("Keep");
+	public static void initGUI()
 	{
+		Random random = new Random();	
+		//beginning of the bit that makes the jFrame
 		JFrame frame = new JFrame("Frame"); //creates the jframe
 		frame.setUndecorated(true);
 		frame.getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
@@ -15,14 +32,13 @@ public class Pig
 		GridBagLayout progLayout = new GridBagLayout();
 		frame.setLayout(progLayout);
 		
-		JLabel userOneLabel = new JLabel("User One");
+
 			userOneLabel.setForeground(Color.RED);
-		JLabel currentRoll = new JLabel("0");
-		JLabel userTwoLabel = new JLabel("User Two");
+			currentPot.setForeground(Color.RED);
+			currentRoll.setForeground(Color.RED);
+
 			userTwoLabel.setForeground(Color.BLUE);
-		JLabel CurrentPot = new JLabel("Pot");
-		JButton rollButton = new JButton("Roll Again");
-		JButton takeButton = new JButton("Keep");
+
 		
 		GridBagConstraints userOneCon = new GridBagConstraints();
 		userOneCon.gridx = 0;
@@ -47,7 +63,7 @@ public class Pig
 		GridBagConstraints potCon = new GridBagConstraints();
 		potCon.gridx = 1;
 		potCon.gridy = 1;
-		frame.add(CurrentPot, potCon);
+		frame.add(currentPot, potCon);
 		
 		GridBagConstraints takeCon = new GridBagConstraints();
 		takeCon.gridx = 2;
@@ -56,10 +72,80 @@ public class Pig
 		
 		frame.pack(); //resizes the frame
 		frame.setVisible(true); //shows the frame
+		
+		rollButton.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						onRoll(random);
+					}
+				});
+		
+		takeButton.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						onTake();
+					}
+				});
+		//end of the bit that makes the jFrame
 	}
-	
 	public static void main(String [] args)
+	{	
+		initGUI();
+	}
+	public static void onRoll(Random random)
 	{
-		initGUI(); //creates the GUI
+		takeButton.setEnabled(true);
+		currentRollValue = random.nextInt(6) + 1;
+		potValue += currentRollValue;
+		currentRoll.setText(currentRollValue + "");
+		currentPot.setText(potValue + "");
+		if(currentRollValue == 1)
+		{
+			potValue = 0;
+			onTake();
+		}
+	}
+	public static void onTake()
+	{
+		if(currentUser)
+		{
+			userOneScore += potValue;
+			userOneLabel.setText(userOneScore + "");
+		}else
+		{
+			userTwoScore += potValue;
+			userTwoLabel.setText(userTwoScore + "");
+		}
+		
+		currentUser = !currentUser;
+		takeButton.setEnabled(false);
+		
+		if(currentUser) //the bit that tells the users who's turn it is
+		{
+			currentRoll.setForeground(Color.RED); //sets the colour of the roll to the active player
+			currentPot.setForeground(Color.RED); //sets the colour of the pot
+		}else 
+		{
+			currentRoll.setForeground(Color.BLUE);
+			currentPot.setForeground(Color.BLUE);
+		}
+		
+		potValue = 0;
+		
+		currentRoll.setText(currentRollValue + "");
+		currentPot.setText(potValue + "");
+		
+		if(userOneScore > 100)
+			onWin(1);
+		if(userTwoScore > 100)
+			onWin(2);
+	}
+	public static void onWin(int winner)
+	{
+		String winnerName = winner == 1 ? "User One" : "User Two";
+		System.out.println(winnerName + " won, the final score was " + userOneScore + ":" + userTwoScore);
+		System.exit(0);
 	}
 }
